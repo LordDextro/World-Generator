b35ca30d9e62d5520529719c45cd04f6f6408191
LordDextro <emil.loock@gmail.com>
2015-01-30 14:12:07 +0100

    Create SolarSystem.h

---
+++ SolarSystem.h
@@ -0,0 +1,57 @@
+#pragma once
+#include <iostream>
+#include <time.h>
+#include <string>
+#include <fstream>
+
+using namespace std;
+
+class SolarSystem
+{
+public:
+
+	SolarSystem();
+	~SolarSystem();
+
+	//Functions:
+	void Star(int star[], int absolute, string SSname);
+	void PlanetNumbersTypes(int star[], string SSname, int absolute, int numberOfPlanets[]);
+
+private:
+	//Class Global variables:
+
+	int absolute;
+	string SSname;
+
+	//-------------------//
+
+
+	//Star:
+	int star[4];
+	enum starType
+	{
+		Solo = 0,
+		Binary = 1,
+		Eclipsing = 2,
+		Giant = 3
+	};
+
+	//-------------------//
+
+	//Planets, types and quantity:
+
+	int numberOfPlanets[4];
+
+	enum planetClass
+	{
+		ClassM = 0,
+		ClassL = 1,
+		ClassD = 2,
+		ClassK = 3
+	};
+
+	//--------------------//
+
+
+};
+
