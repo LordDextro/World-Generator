c9e31a62f499b32bb7ec096cdfb769e905cfad37
LordDextro <emil.loock@gmail.com>
2015-01-30 14:10:25 +0100

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <fstream>
+#include "SolarSystem.h"
+
+using namespace std;
+
+int main()
+{
+
+	SolarSystem Sys;
+
+	{
+		Sys;
+	}
+	system("pause");
+	return 0;
+}
